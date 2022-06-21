997620bbf4783306d905f2d69e526e167c22b3b1
A-SCharles <a.charles.edu@gmail.com>
2022-06-21 08:18:39 +0200

    added

--- script/script.js
+++ script/script.js
@@ -1,50 +1,47 @@
-const numbers = document.getElementById('numbers')
-const values = document.getElementById('values')
-const nextNo = document.getElementById('next')
-numbers.focus()
-let arr = [1, 4, 7, 2, 5, 9, 3];
+const values = document.getElementById("values");
+const nextNo = document.getElementById("next");
+numbers.focus();
+let arr = [];
 
-const btnadd = document.getElementById('add')
-btnadd.addEventListener('click', () => {
-    // Adds numbers to array
-    let i = arr.length;
-    arr[i] = numbers.value
-    if (i >= 1) {
-        values.innerHTML += ', '
-    }
-    values.innerHTML += (arr[i])
-    numbers.focus()
-    numbers.value = ''
-    i++;
-})
+const btnaddFalse = document.getElementById("false");
+btnaddFalse.addEventListener("click", () => {
+    // Adds False to array
+    arr.push('False')
+    values.innerHTML = arr
+});
 
-// adds numbers to array on enter 
+const btnaddTrue = document.getElementById("true");
+btnaddTrue.addEventListener("click", () => {
+  // Adds True to array
+  arr.push("True");
+  values.innerHTML = arr;
+});
+
+// adds numbers to array on enter
 numbers.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault()
-        btnadd.click()
-    }
-})
+  if (event.key === "Enter") {
+    event.preventDefault();
+    btnadd.click();
+  }
+});
 
-const btnCalc = document.getElementById('calculate')
-btnCalc.addEventListener('click', () => {
+const btnCalc = document.getElementById("calculate");
+btnCalc.addEventListener("click", () => {
 
-    const iterator = arr.values();
-    let x = iterator.next().value;
-    nextNo.innerHTML += x 
-})
+});
 
-const btnReset = document.getElementById('reset')
-btnReset.addEventListener('click', () => {
-    numbers.value = '';
-    values.innerHTML = '';
-    nextNo.innerHTML = '';
-    arr = [1, 4, 7, 2, 5, 9, 3]
-})
+const btnReset = document.getElementById("reset");
+btnReset.addEventListener("click", () => {
+  values.innerHTML = "";
+  nextNo.innerHTML = "";
+  arr = [];
+});
 
 //copyright
-let year = new Date().getFullYear()
-document.getElementById('copyright').innerHTML = '&copyAbdus-Samad Charles ' + year;
-document.getElementById('copyright').innerHTML = '&copyAbdus-Samad Charles ' + year;
+let year = new Date().getFullYear();
+document.getElementById("copyright").innerHTML =
+  "&copyAbdus-Samad Charles " + year;
+document.getElementById("copyright").innerHTML =
+  "&copyAbdus-Samad Charles " + year;
 
 // [1,4,7,2,5,9,3];
